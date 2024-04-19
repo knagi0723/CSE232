ebbebff95e05ee07207deb2f051ab0d64d9f696b
Krizma Nagi <63876662+knagi0723@users.noreply.github.com>
2024-04-19 13:23:43 -0400

    Lab 1 Uploaded
    Lab 1: Input & Output

---
+++ lab-01/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+
+{
+  std::cout << "My name is Krizma Nagi. I read the syllabus and I understand the consequences of academic dishonesty." << std::endl;
+  
+  return 0;
+}
